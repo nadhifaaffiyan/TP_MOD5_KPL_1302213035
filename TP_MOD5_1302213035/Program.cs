d5976b3f9539278d8cf98d4efab2542fec8a1ffe
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-03-19 19:17:52 +0700

    tp5kpl

--- TP_MOD5_1302213035/Program.cs
+++ TP_MOD5_1302213035/Program.cs
@@ -1,5 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Hello World");
-Console.WriteLine("NAMA SAYA");
+class Program
+{
+    void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("NIM praktikan"); // mengganti dengan NIM praktikan
+        data.PrintData();
+    }
+}
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+class program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("NADHIFA AFFIYAN ZALWI");
+    }
+
+
+}
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+}
